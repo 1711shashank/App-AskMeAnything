88101ba48c47682fc6329a8ce32565f84e042d36
Kumar Shashank <1711shashank@gmail.com>
2020-05-19 15:47:25 +0530

    Customised
    Signed-off-by: Kumar Shashank <1711shashank@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,44 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-void main() {
-  return runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.lightBlue,
-        appBar: AppBar(
-          title: Text('Ask Me Anything'),
-          backgroundColor: Colors.blue.shade800,
-        ),
-        body: DicePage(),
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
       ),
-    ),
-  );
-}
+    );
 
-class DicePage extends StatefulWidget {
+class BallPage extends StatelessWidget {
   @override
-  _DicePageState createState() => _DicePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
 }
 
-class _DicePageState extends State<DicePage> {
-  int num = 1;
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
 
-  void Roll() {
-    setState(() {
-      num = Random().nextInt(6) + 1;
-    });
-  }
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                Roll();
-              },
-              child: Image.asset('images/ball$num.png'),
-            ),
-          ),
-        ],
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }
